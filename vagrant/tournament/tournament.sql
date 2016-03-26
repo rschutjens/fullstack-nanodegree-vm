74fe9298a60f3181fba6dc42c86d45a0450b25c7
Ron <rschutjens@outlook.com>
2016-03-26 18:41:38 -0400

    Add players and matches tables to tournament

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,16 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Contains all players and gives them an ID.
+create table players (
+  id serial primary key,
+  name text
+);
 
+-- Table containing all matches, match ID as players can face off in different
+-- matches against eachother, players need to be in players table.
+create table matches (
+  id serial primary key,
+  win integer references players(id),
+  loss integer references players(id)
+);
